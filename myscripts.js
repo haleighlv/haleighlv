4e101eafa33739b45a77cac02843ed22759668d2
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-08-05 12:48:05 +1000

    updates

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,27 @@
+// carousel element - homepage
 
 
-function myFunction() {
-  var x = document.getElementById("myMobilenav");
-  if (x.className === "mobilenav") {
-    x.className += " responsive";
-  } else {
-    x.className = "mobilenav";
-  }
-}
+
+
+
+
+ // element.addEventListener("click", function (); });
+// responsive navigation
+$(document).ready(function () {
+  $(".hamburger-icon").click(function () {
+      
+    // this updates the class name of .mobile-nav to .mobile-nav.open 
+    
+    $(".mobile-nav").toggleClass("open");
+
+    // you'll need to add a selector in css to define what .mobile-nav.open looks like
+    $(".mobile-nav").toggleClass("closed")
+    $(".hamburger-icon").toggleClass("closed")
+    // add here a toggle to make the hamburger disappear on this click as well
+    // eg $(".hamburger-icon").toggleClass("closed")
+    // then define .hamburger-icon.closed to eg display: none in Css
+    
+
+    });
+});
+
