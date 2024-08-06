fa1832e307268ef3ba9a7b54cd96c70e6c8d17fa
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-08-07 09:09:01 +1000

    redo css

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,28 @@
 
-//  // element.addEventListener("click", function (); });
-// // responsive navigation
+ // element.addEventListener("click", function (); });
+// responsive navigation
+$(document).ready(function () {
+  $(".hamburger-icon").click(function () {
+    // this updates the class name of .mobile-nav to .mobile-nav.open 
+    
+    $(".mobile-nav").toggleClass("open");
+
+    // you'll need to add a selector in css to define what .mobile-nav.open looks like
+    $(".mobile-nav").toggleClass("closed");
+    $(".hamburger-icon").toggleClass("closed");
+    // add here a toggle to make the hamburger disappear on this click as well
+    // eg $(".hamburger-icon").toggleClass("closed")
+    // then define .hamburger-icon.closed to eg display: none in Css
+    
+
+  });
+});
+
+//
+
+// // responsive navigatioon
 // $(document).ready(function () {
-//   $(".hamburger-icon").click(function () {
-      
-//     // this updates the class name of .mobile-nav to .mobile-nav.open 
-    
-//     $(".mobile-nav").toggleClass("open");
-
-//     // you'll need to add a selector in css to define what .mobile-nav.open looks like
-//     $(".mobile-nav").toggleClass("closed");
-//     $(".hamburger-icon").toggleClass("closed");
-//     // add here a toggle to make the hamburger disappear on this click as well
-//     // eg $(".hamburger-icon").toggleClass("closed")
-//     // then define .hamburger-icon.closed to eg display: none in Css
-    
-
-//   });
+//     $(".hamburger-icon").click(function () {
+//         $(".mobile-nav").toggleClass("open");
+//     });
 // });
-
-// //
